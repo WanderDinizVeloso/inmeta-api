5f8a75c25d0f74525e33444f83b0ed833cb7a3f9
Wander Veloso <wanveloso@yahoo.com.br>
2026-07-31 20:57:22 -0300

    feat: add documentTypes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './shared/infra/prisma/prisma.module';
 import { EmployeesModule } from './modules/employees/employees.module';
+import { DocumentTypesModule } from './modules/document-types/document-types.module';
 
 @Module({
-  imports: [PrismaModule, EmployeesModule],
+  imports: [PrismaModule, EmployeesModule, DocumentTypesModule],
 })
 export class AppModule {}
