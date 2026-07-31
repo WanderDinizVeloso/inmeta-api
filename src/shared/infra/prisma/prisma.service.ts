42997240e272e35fb613a8e55acfb0435e93f95f
Wander Veloso <wanveloso@yahoo.com.br>
2026-07-31 18:23:33 -0300

    chore: adds database_url validation to the prisma service

--- src/shared/infra/prisma/prisma.service.ts
+++ src/shared/infra/prisma/prisma.service.ts
@@ -8,14 +8,24 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly pool: Pool;
+
   constructor() {
     const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error(
+        'FATAL: DATABASE_URL não está definida nas variáveis de ambiente.',
+      );
+    }
 
     const pool = new Pool({ connectionString });
 
     const adapter = new PrismaPg(pool);
 
     super({ adapter });
+
+    this.pool = pool;
   }
 
   async onModuleInit() {
@@ -24,5 +34,6 @@ export class PrismaService
 
   async onModuleDestroy() {
     await this.$disconnect();
+    await this.pool.end();
   }
 }
