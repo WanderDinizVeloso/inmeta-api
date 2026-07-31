a2a70e455a60310ec9a1ffd546f56206ea71d998
Wander Veloso <wanveloso@yahoo.com.br>
2026-07-31 18:55:37 -0300

    feat: register listEmployees in controller and module

--- src/modules/employees/api/controllers/employees.controller.ts
+++ src/modules/employees/api/controllers/employees.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateEmployeeUseCase } from '../../application/use-cases/create-employee.use-case';
 import { CreateEmployeeDto } from '../dtos/create-employee.dto';
 import { EmployeeResponseDto } from '../dtos/employee-response.dto';
 import { ApiDocsCreateEmployee } from '../docs/create-employee.doc';
+import { ListEmployeesUseCase } from '../../application/use-cases/list-employees.use-case';
+import { ApiDocsListEmployees } from '../docs/list-employees.doc';
+import { PaginationQueryDto } from '../../../../shared/dtos/pagination-query.dto';
 
 @ApiTags('Colaboradores')
 @Controller('employees')
 export class EmployeesController {
-  constructor(private readonly createEmployeeUseCase: CreateEmployeeUseCase) {}
+  constructor(
+    private readonly createEmployeeUseCase: CreateEmployeeUseCase,
+    private readonly listEmployeesUseCase: ListEmployeesUseCase,
+  ) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
@@ -22,5 +36,12 @@ export class EmployeesController {
       email: employee.email,
       createdAt: employee.createdAt,
     };
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiDocsListEmployees()
+  async findAll(@Query() query: PaginationQueryDto) {
+    return this.listEmployeesUseCase.execute(query);
   }
 }
--- src/modules/employees/employees.module.ts
+++ src/modules/employees/employees.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EmployeesController } from './api/controllers/employees.controller';
 import { CreateEmployeeUseCase } from './application/use-cases/create-employee.use-case';
 import { EmployeeRepository } from './infra/repositories/employee.repository';
+import { ListEmployeesUseCase } from './application/use-cases/list-employees.use-case';
 
 @Module({
   controllers: [EmployeesController],
-  providers: [EmployeeRepository, CreateEmployeeUseCase],
+  providers: [EmployeeRepository, CreateEmployeeUseCase, ListEmployeesUseCase],
 })
 export class EmployeesModule {}
